9539885085977a03f6875a0eadd8802fd1669428
Jerry Iu <jerryiu424@hotmail.com>
2019-08-24 18:49:42 -0400

    Resize any uploaded image to fit canvas size

--- scripts.js
+++ scripts.js
@@ -13,55 +13,63 @@ const uploadButton = document.getElementById('upload');
 clearCanvas();
 
 function clearCanvas() {
-  context.beginPath();
-  context.rect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "white";
-  context.fill();
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "white";
+    context.fill();
 }
 
 function handleFiles(files) {
-  const file = files[0];
-  const img = new Image;
-  img.onload = function () {
-    context.drawImage(img, 0, 0);
-  }
-  img.src = URL.createObjectURL(file);
-  console.log(file);
+    const file = files[0];
+    const img = new Image;
+    resizeImage(img, 320, 240);
+    img.src = URL.createObjectURL(file);
+    console.log(file);
 }
+
+function resizeImage(image, width, height) {
+    const img = image;
+    img.onload = () => {
+        img = greyscaleImage(img);
+        context.drawImage(img, 0, 0, width, height);
+    }
+}
+
+
 
 // STATE MACHINE!!!!!
 setInterval(() => {
-  video.style.display = state === 'recording' ? null : 'none';
-  canvas.style.display = state === 'recording' ? 'none' : null;
-  takePhotoButton.style.display = state === 'confirm' ? 'none' : null;
-  takePhotoButton.children[0].textContent = state === 'recording' ? 'photo_camera' : 'add_a_photo';
-  confirmButton.style.display = state === 'confirm' ? null : 'none';
-  resetButton.style.display = state === 'idle' ? 'none' : null;
-  resetButton.children[0].textContent = state === 'recording' ? 'clear' : 'undo';
-  uploadButton.style.display = state === 'idle' ? null : 'none';
+    video.style.display = state === 'recording' ? null : 'none';
+    canvas.style.display = state === 'recording' ? 'none' : null;
+    takePhotoButton.style.display = state === 'confirm' ? 'none' : null;
+    takePhotoButton.children[0].textContent = state === 'recording' ? 'photo_camera' : 'add_a_photo';
+    confirmButton.style.display = state === 'confirm' ? null : 'none';
+    resetButton.style.display = state === 'idle' ? 'none' : null;
+    resetButton.children[0].textContent = state === 'recording' ? 'clear' : 'undo';
+    uploadButton.style.display = state === 'idle' ? null : 'none';
 }, 50);
 
 // Trigger photo take
-takePhotoButton.addEventListener("click", function () {
-  if (state === 'recording') {
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    video.srcObject = null;
-    video.pause();
-    state = 'confirm';
-  } else {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-      });
+takePhotoButton.addEventListener("click", function() {
+    if (state === 'recording') {
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        video.srcObject = null;
+        video.pause();
+        state = 'confirm';
+    } else {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+            });
+        }
+        state = 'recording';
     }
-    state = 'recording';
-  }
 });
 
-resetButton.addEventListener("click", function () {
-  video.srcObject = null;
-  video.pause();
-  clearCanvas();
-  state = 'idle';
+resetButton.addEventListener("click", function() {
+    video.srcObject = null;
+    video.pause();
+    clearCanvas();
+    state = 'idle';
 });
