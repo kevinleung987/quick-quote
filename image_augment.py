401210f9403719437735bd76150c1416e29cb001
GabrielGelgor <gabriel.gelgor@uoit.net>
2019-08-24 12:38:37 -0400

    image augmentation pre-test complete

--- image_augment.py
+++ image_augment.py
@@ -1,4 +1,5 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import os, random
 
 datagen = ImageDataGenerator(              #Parameters by which we will randomly transform images, thereby increasing data set size
     rotation_range=40,                     #Image rotation range in degrees
@@ -9,4 +10,13 @@ datagen = ImageDataGenerator(              #Parameters by which we will randomly
     horizontal_flip=True,                  #random horizontal flip half the images
     fill_mode='nearest')                   #Fill in newly created pixels from rotation
 
+img = load_img(str(random.choice(os.listdir("dataset/greyscale/1"))))
+img_array = img_to_array(img)                                           #Numpy array with shape of 3, 150, 150
+img_array = img_array.reshape((1,) + img_array.shape)                   #Numpy array with shape 1, 3, 150, 150
+
+i = 0
+for batch in datagen.flow(img_array, batch_size=1,save_to_dir='preview',save_prefix='car', save_format='jpeg'):
+    i += 1
+    if i > 20:
+        break   #would cause indefinite loop otherwise.
 
