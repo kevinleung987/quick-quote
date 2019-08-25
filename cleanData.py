e184c1cf1c1a9656af5d06b7e5f0ef3385baf21d
Jerry Iu <jerryiu424@hotmail.com>
2019-08-24 20:32:46 -0400

    Fixed my dumb calculation mistake

--- cleanData.py
+++ cleanData.py
@@ -4,15 +4,17 @@ import glob, os
 f= open("train.csv","a+")
 f.write("label,")
 
-for i in range(1567):
+for i in range(783):
     f.write("Pixel "+str(i+1)+",")
-f.write("Pixel 1567\n")
+f.write("Pixel 783\n")
 
 os.chdir("dataset\\greyscale\\train\\")
 for file in glob.glob("low*.png"):
     img = Image.open(file,'r')
     pix_val = list(img.getdata())
-    pix_val_flat = [x for sets in pix_val for x in sets]
+    pix_val_flat = []
+    for sets in pix_val:
+        pix_val_flat.append(sets[0])
     f.write("0,")
     for i in range(len(pix_val_flat)):
         f.write(str(pix_val_flat[i]) + ",")
@@ -21,7 +23,9 @@ for file in glob.glob("low*.png"):
 for file in glob.glob("med*.png"):
     img = Image.open(file,'r')
     pix_val = list(img.getdata())
-    pix_val_flat = [x for sets in pix_val for x in sets]
+    pix_val_flat = []
+    for sets in pix_val:
+        pix_val_flat.append(sets[0])
     f.write("1,")
     for i in range(len(pix_val_flat)):
         f.write(str(pix_val_flat[i]) + ",")
@@ -30,7 +34,9 @@ for file in glob.glob("med*.png"):
 for file in glob.glob("high*.png"):
     img = Image.open(file,'r')
     pix_val = list(img.getdata())
-    pix_val_flat = [x for sets in pix_val for x in sets]
+    pix_val_flat = []
+    for sets in pix_val:
+        pix_val_flat.append(sets[0])
     f.write("2,")
     for i in range(len(pix_val_flat)):
         f.write(str(pix_val_flat[i]) + ",")
